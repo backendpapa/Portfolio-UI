75c31784372e10a66ea690c71b2a3f69a2858e6e
Akeju Paul <akejupaul10@gmail.com>
2021-10-15 00:37:31 +0100

    portfolio ui

---
+++ src/components/Project.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+      <v-row no-gutters>
+          <p class="text-h6 font-weight-bold">Projects</p>
+          <v-spacer></v-spacer>
+          <p class="text-h6 grey--text">October 2021</p>
+      </v-row>
+      <v-row no-gutters>
+          <v-col cols="10">
+              <!-- stat -->
+              <v-row no-gutters>
+                  <v-col cols="6" xl="3" lg="3" >
+                      <div>
+                         <p class="text-body-1 font-weight-bold" style="color:#736FC6">45</p> 
+                         <p class="text-caption mt-n4 font-weight-bold grey--text">In Progress</p>
+                      </div>
+                  </v-col>
+                   <v-col cols="6" xl="3" lg="3">
+                      <div>
+                         <p class="text-body-1 font-weight-bold" style="color:#F2A684">12</p> 
+                         <p class="text-caption mt-n4 font-weight-bold grey--text">In Progress</p>
+                      </div>
+                  </v-col>
+                  <v-col cols="6" xl="3" lg="3">
+                      <div>
+                         <p class="text-body-1 font-weight-bold" style="color:#F6C05F">10</p> 
+                         <p class="text-caption mt-n4 font-weight-bold grey--text">Upcoming</p>
+                      </div>
+                  </v-col>
+                   <v-col cols="6" xl="3" lg="3">
+                      <div>
+                         <p class="text-body-1 font-weight-bold" style="color:#52F8F3">64</p> 
+                         <p class="text-caption mt-n4 font-weight-bold grey--text">Total Project</p>
+                      </div>
+                  </v-col>
+              </v-row>
+          </v-col>
+          <v-col cols="2">
+              <v-row no-gutters>
+              
+                  <v-btn icon plain><v-icon>mdi-view-list-outline</v-icon></v-btn>
+                  <v-btn icon plain><v-icon>mdi-view-grid</v-icon></v-btn>
+              </v-row>
+          </v-col>
+      </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,29 @@
 <template>
-  <div></div>
+  <div>
+   <v-container>
+     
+        <v-row no-gutters style="min-height:100vh" >
+          <v-col  cols="12" xl="8" lg="8">
+            <v-card style="border-radius:15px;height:100%" class="pa-5">
+            <Project />
+          </v-card>
+          </v-col>
+          <v-col cols="12" xl="4" lg="4"></v-col>
+        
+    </v-row>
+   
+   </v-container>
+  </div>
 </template>
 
 <script>
 
-
+import Project from '../components/Project.vue'
   export default {
     name: 'Home',
 
     components: {
-     
+     Project
     },
   }
 </script>
